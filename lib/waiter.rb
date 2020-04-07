71abcd08716601bc103bab77f0fbf7a6ec2070a3
Justin Winger <justinwinger4@gmail.com>
2020-04-07 17:22:08 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
+  attr_accessor :name, :years
+  
+  def initialize(name, years)
+    @name = name
+    @years = years 
+    @@all = self 
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
