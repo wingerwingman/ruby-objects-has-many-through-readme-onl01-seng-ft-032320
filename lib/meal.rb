6647cc46a363759aa00574de9378efb87b4e8e91
Justin Winger <justinwinger4@gmail.com>
2020-04-07 17:26:26 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+  attr_accessor :waiter, :customer, :tip 
+  @@all = [] 
+  
+  def initialize(waiter, customer, tip = 0)
+    @waiter = waiter 
+    @customer = customer
+    @tip = tip 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end
+  
 
 end
